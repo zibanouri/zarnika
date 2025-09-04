f13414cd54257a6b195017b4d0b4f2d22fa44031
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-04 21:03:00 +0330

    initializing dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+return(
+    <h1 className="text-yellow-300">Zarnika | Admin Panel</h1>
+)
+}
+
+
+export default Dashboard;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,7 @@
+import Dashboard from "../components/Dashboard.tsx";
 const Index = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-rose-800">
-      Zarnika | Admin Panel
-    </h1>
+    <Dashboard />
   );
 };
 
