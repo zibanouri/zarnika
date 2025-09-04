5dbbf521786f1486c6ddcbf577203e31821f6bca
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-04 20:43:44 +0330

    add tailwind configs to project

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,9 @@
 const Index = () => {
-  return <h1>Zarnika | Admin Panel</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline text-rose-800">
+      Zarnika | Admin Panel
+    </h1>
+  );
 };
 
 export default Index;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base:"zarnika",
-  plugins: [react()],
+  plugins: [
+    react(),
+     tailwindcss()
+    ],
 })
