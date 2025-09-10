1ae05d52f6c5ab61e4274f4c40661312963e6bd6
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-10 12:29:45 +0330

    add lucide

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,7 +1,8 @@
 const DashboardHeader = () =>{
     return(
         <header className="fixed top-0 left-0 right-0 z-50 h-16 bg-pink-50 border border-b text-pink-800 hover:text-pink-700 duration-300">
-
+          <div className="flex items-center justify-between h-full px-4"></div>
+          <div className="flex items-center gap-4"></div>
         </header>
     )
 }
