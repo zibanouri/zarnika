5f1cf6754ba0f26bc3c8ae1d074066bdfdf61fb0
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-11 08:43:29 +0330

    initialize shadcn/ui with neutral theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from "path"
 
 export default defineConfig({
-  base:'/zarnika/',
+  base: '/cores/',
   plugins: [
     react(),
-     tailwindcss()
-    ],
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
