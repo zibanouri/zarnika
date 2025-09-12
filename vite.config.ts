3f89eb97dcc9d758a39e339eec33b80c8258514d
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-12 10:17:43 +0330

    update Vite config for CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 export default defineConfig({
-  base: '/cores/',
+  base: '/zarnika/',
   plugins: [
     react(),
     tailwindcss(),
