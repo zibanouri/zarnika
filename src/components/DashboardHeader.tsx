08a415b66e33a8297ce49ff412d26a3813fa75bc
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-13 06:06:47 +0330

    add button to DashboardHeade

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,8 +1,13 @@
+import { Button } from "./ui/button";
 const DashboardHeader = () =>{
     return(
         <header className="fixed top-0 left-0 right-0 z-50 h-16 bg-pink-50 border border-b text-pink-800 hover:text-pink-700 duration-300">
           <div className="flex items-center justify-between h-full px-4"></div>
-          <div className="flex items-center gap-4"></div>
+          <div className="flex items-center gap-4">
+            <Button>
+              
+            </Button>
+          </div>
         </header>
     )
 }
