fa5056dbb6962abcad749fc2b1aae5d5d58835dc
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-13 06:49:08 +0330

    add mobile sidebar button

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,13 +1,16 @@
 import { Button } from "./ui/button";
+import {Menu} from "lucide-react"
 const DashboardHeader = () =>{
     return(
-        <header className="fixed top-0 left-0 right-0 z-50 h-16 bg-pink-50 border border-b text-pink-800 hover:text-pink-700 duration-300">
-          <div className="flex items-center justify-between h-full px-4"></div>
-          <div className="flex items-center gap-4">
-            <Button>
-              
-            </Button>
-          </div>
+       <header className="fixed top-0 left-0 right-0 h-16 z-50 bg-pink-100 border-b border-pink-600 hover:border-pink-400 duration-300">
+            <div className="flex items-center justify-between h-full px-4">
+                {/* Left Section */}
+                <div className="flex items-center gap-4">
+                    <Button variant="ghost" size="sm" className="md:hidden">
+                    <Menu className="h-5 w-5"/>
+                    </Button>
+                </div>
+                </div>
         </header>
     )
 }
