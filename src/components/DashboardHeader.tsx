99e9ed1b41c5381fd9bbcc181eea664d56e17b47
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-14 19:33:14 +0330

    update: modify DashboardHeader.tsx and main.tsx

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -12,26 +12,26 @@ import {
 
 const DashboardHeader = () => {
     return (
-        <header className="fixed top-0 left-0 right-0 h-16 z-50 bg-pink-100 border-b border-pink-600 hover:border-pink-400 duration-300">
+        <header className="fixed top-0 left-0 right-0 h-16 z-50 bg-pink-100 border-b border-slate-600 hover:border-slate-400 duration-300">
             <div className="flex items-center justify-between h-full px-4">
                 {/* Left Section */}
                 <div className="flex items-center gap-4">
-                    <Button variant="ghost" size="sm" className="md:hidden">
+                    <Button variant="ghost" size="sm" className="md:hidden rounded-full">
                         <Menu className="h-5 w-5" />
                     </Button>
                 </div>
                 {/* Right Section */}
                 <div className="flex items-center gap-2">
-                    <Button variant="ghost" size="sm">
+                    <Button variant="ghost" size="sm" className="rounded-full">
                         <Moon className="h-5 w-5" />
                     </Button>
-                    <Button variant="ghost" size="sm" className="relative">
+                    <Button variant="ghost" size="sm" className="relative rounded-full">
                         <Bell className="h-5 w-5" />
                         <Badge className="absolute -top-1 -right-1 h-4 w-4 rounded-full p-0 bg-red-400 flex items-center justify-center">
-                            28
+                            3
                         </Badge>
                     </Button>
-                    <Button variant="ghost" size="sm">
+                    <Button variant="ghost" size="sm" className="rounded-full">
                         <Settings className="h-5 w-5" />
                     </Button>
                     <DropdownMenu>
@@ -39,14 +39,14 @@ const DashboardHeader = () => {
                             <Button
                                 variant="ghost"
                                 size="sm"
-                                className="relative"
+                                className="relative rounded-full"
                             >
                                 <Avatar className="h-8 w-8">
                                     <AvatarImage
                                         src="public/avatar/profile.webp"
                                         alt="profile"
                                     />
-                                    <AvatarFallback>FA</AvatarFallback>
+                                    <AvatarFallback>ZN</AvatarFallback>
                                 </Avatar>
                             </Button>
                         </DropdownMenuTrigger>
