70bc260db8e5997d4b1d832e66770c99a0802ac1
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-18 15:12:04 +0330

    add dashboardsidebar in code

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,14 @@
 import Footer from './Footer';
 import DashboardHeader from './DashboardHeader';
+import DashboardSidebar from './DashboardSidebar'; 
+
 const Dashboard = () => {
     return (
         <div className="min-h-screen">
             <DashboardHeader />
-
+            
             <div className="flex">
-                <div id="DashboardSidebar"></div>
+                <DashboardSidebar />
 
                 <main className="flex-1 transition-all duration-300 ml-64 pt-16 min-h-screen flex flex-col overflow-x-hidden">
                     <div id="DashboardContent" className="flex-1"></div>
---
+++ src/components/DashboardSidebar.tsx
@@ -0,0 +1,9 @@
+const DashboardSidebar = () => {
+    return (
+        <aside className="fixed left-0 top-16 z-50 h-[calc(100vh-4rem)] bg-background border-pink-200 hover:border-pink-400 overflow-hidden">
+
+        </aside>
+    )
+}
+
+export default DashboardSidebar;
