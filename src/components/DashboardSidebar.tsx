e7b3aa9af3655f14cbf8b6909483cce49bdac818
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-18 15:23:57 +0330

    initializing sidebar

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -1,6 +1,6 @@
 const DashboardSidebar = () => {
     return (
-        <aside className="fixed left-0 top-16 z-50 h-[calc(100vh-4rem)] bg-background border-pink-200 hover:border-pink-400 overflow-hidden">
+        <aside className="fixed left-0 top-16 z-50 h-[calc(100vh-4rem)] bg-background border-r border-pink-300 hover:border-pink-400 duration-300 overflow-hidden w-64">
 
         </aside>
     )
