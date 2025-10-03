83de18a5a1ae44c2cc6e706834a048bfa11dc641
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-04 02:53:44 +0330

    add styles separator ui in code

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -96,6 +96,16 @@ const LoginPage = () => {
                                 Sign In
                             </Button>
                         </form>
+                        <div className="relative">
+                            <div className="absolute inset-0 flex items-center">
+                                <Separator className="w-full" />
+                            </div>
+                            <div className="relative flex justify-center text-xs uppercase">
+                                <span className="bg-card px-2 text-muted-foreground">
+                                    Or continue with
+                                </span>
+                            </div>
+                        </div>
                     </CardContent>
                 </Card>
             </div>
