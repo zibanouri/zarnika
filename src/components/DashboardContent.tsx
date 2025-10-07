9e98dfc7947d4c3ff55a4f9970c95fdf292c6a6e
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-07 21:11:10 +0330

    add DashboardContent components in project

---
+++ src/components/DashboardContent.tsx
@@ -0,0 +1,10 @@
+interface DashboardContentProps{
+currentPage:string;
+}
+const DashboardContent = ({currentPage}:DashboardContentProps) => {
+   const renderContent = () => {
+
+   }
+}
+
+export default DashboardContent;
