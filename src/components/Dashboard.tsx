824e1335163668d29bc0ce9a4d8e8b710b2e7420
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-07 23:05:06 +0330

    add currentPage state to dashboard content

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,23 @@
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 import Footer from './Footer';
 import DashboardHeader from './DashboardHeader';
 import DashboardSidebar from './DashboardSidebar';
+import DashboardContent from './DashboardContent';
 
 const Dashboard = () => {
+   const currentPage = 'dashboard';
+    // const navigate = useNavigate();
+
+    // const handelPageChange = (page: string) => {
+    //     setCurrentPage(page);
+    //     switch (page) {
+    //         case "dashboard":
+    //         default:
+    //             navigate("/");
+    //             break
+    //     }
+    // };
     return (
         <div className="min-h-screen">
             <DashboardHeader />
@@ -11,7 +26,9 @@ const Dashboard = () => {
                 <DashboardSidebar />
 
                 <main className="flex-1 transition-all duration-300 ml-64 pt-16 min-h-screen flex flex-col overflow-x-hidden">
-                    <div id="DashboardContent" className="flex-1"></div>
+                    <div id="DashboardContent" className="flex-1">
+                        <DashboardContent currentPage={currentPage} />
+                    </div>
                     <Footer />
                 </main>
             </div>
