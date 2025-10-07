4cee52bc444f1ebc1c031ecea0f9b75c6bd97625
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-07 21:53:35 +0330

    correct DashboardContentProps interface

--- src/components/DashboardContent.tsx
+++ src/components/DashboardContent.tsx
@@ -1,10 +1,22 @@
-interface DashboardContentProps{
-currentPage:string;
+interface DashboardContentProps {
+    currentPage: string;
 }
-const DashboardContent = ({currentPage}:DashboardContentProps) => {
-   const renderContent = () => {
-
-   }
+const DashboardContent = ({ currentPage }: DashboardContentProps) => {
+    const renderContent = () => {
+        switch (currentPage) {
+            default:
+                return (
+                    <div className="space-6">
+                        This id dashboard content
+                    </div>
+                )
+            }
+    }
+    return (
+<div className="p-6 animate-fade-in">
+{renderContent()}
+</div>
+    )
 }
 
 export default DashboardContent;
