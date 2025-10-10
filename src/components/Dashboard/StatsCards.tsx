2f035536a0ce633c70cd07b907c166e39728e0d3
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-10 15:23:07 +0330

    update stats cards component in project

--- src/components/Dashboard/StatsCards.tsx
+++ src/components/Dashboard/StatsCards.tsx
@@ -1,3 +1,39 @@
+import { CreditCard, Users, Target, ShoppingCart } from 'lucide-react';
+const Stats = [
+    {
+        title: 'Total',
+        value: '$324,592',
+        change: '+12.5%',
+        trend: 'up',
+        icon: CreditCard,
+        color: 'text-emerald-600',
+    },
+    {
+        title: 'Action Users',
+        value: '2.845',
+        change: '+10.43',
+        trend: 'up',
+        icon: Users,
+        color: 'text-pink-800',
+    },
+    {
+        title: 'Orders',
+        value: '1543',
+        change: '+9.75',
+        trend: 'Down',
+        icon: ShoppingCart,
+        color: 'text-yellow-500',
+    },
+    {
+        title: 'Rate',
+        value: '3.24%',
+        change: '+5.7%',
+        trend: 'up',
+        icon: Target,
+        color: 'text-purple-500',
+    },
+];
+
 const StatsCards = () => {
     return <div>Statistic cards</div>;
 };
