ca951dbccf4ce7a401f2ff636d2d805881cccb83
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-10 18:39:36 +0330

    update styles welcom section component

--- src/components/Dashboard/WelcomeSection.tsx
+++ src/components/Dashboard/WelcomeSection.tsx
@@ -1,7 +1,7 @@
 const WelcomeSection = () => {
-     return (
+  return (
     <div className="space-y-6">
-      <div className="bg-gradient-to-r from-pink-400 to-pink-300 rounded-2xl p-8 text-white relative overflow-hidden">
+      <div className="bg-gradient-to-r from-pink-400 to-pink-500 rounded-2xl p-8 text-white relative overflow-hidden">
         <div className="relative z-10">
           <h1 className="text-3xl font-bold mb-2">
             It's good to see you back, Ziba!
