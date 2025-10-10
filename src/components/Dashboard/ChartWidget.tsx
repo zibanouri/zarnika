5b55481c99bc641a91ec04ad23c69731b17912d9
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-10 23:28:24 +0330

    add button ui

--- src/components/Dashboard/ChartWidget.tsx
+++ src/components/Dashboard/ChartWidget.tsx
@@ -1,10 +1,15 @@
-import { Card, CardHeader } from '@/components/ui/card';
-
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '../ui/button';
 const ChartWidget = () => {
     return (
         <Card className='bg-red-300'>
-        <CardHeader>
+        <CardHeader className='flex flex-row items-center '>
+      <CardTitle>
+        Performanc Overview
+      </CardTitle>
+      <div className='flex gap-2'>
 
+      </div>
         </CardHeader>
         </Card>
     )
