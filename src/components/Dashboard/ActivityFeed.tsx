e34557830275150b53a18a9900b97f2d1dbae856
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-15 01:09:49 +0330

    add badge ui in code

--- src/components/Dashboard/ActivityFeed.tsx
+++ src/components/Dashboard/ActivityFeed.tsx
@@ -1,6 +1,7 @@
 import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card';
 import { AlarmClock } from 'lucide-react';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { badge } from '@/components/ui/badge';
 const ActivityFeed = () => {
     return (
         <Card>
@@ -10,12 +11,22 @@ const ActivityFeed = () => {
                     Recent Activity
                 </CardTitle>
             </CardHeader>
-            <CardContent className="space-y-4">
+            <CardContent >
                 <div className="space-y-4">
-                    <Avatar className="h-8 w-8 flex-shrink-0">
-                        <AvatarImage src="zarnika/avatar/profile3.png" />
-                        <AvatarFallback>ZN</AvatarFallback>
-                    </Avatar>
+                    <div className='flex items-start gap-2 rounded-lg hover:bg-accent/50 cursor-pointer'>
+                        <Avatar className="h-8 w-8 flex-shrink-0 items-start gap-3">
+                            <AvatarImage src="zarnika/avatar/profile3.png" />
+                            <AvatarFallback>ZN</AvatarFallback>
+                        </Avatar>
+                        <div className='flex-1'>
+                            <div className='flex items-center gap-2 mb-1'>
+                                <span className='text-sm font-mono'>
+                                    Ziba Nouri
+                                </span>
+                            badge
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </CardContent>
         </Card>
