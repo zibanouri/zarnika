0909c0ccfba6d10913822c839ffc30ed23e963de
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-15 00:44:34 +0330

    add avatar ui in code

--- src/components/Dashboard/ActivityFeed.tsx
+++ src/components/Dashboard/ActivityFeed.tsx
@@ -1,5 +1,6 @@
 import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card';
 import { AlarmClock } from 'lucide-react';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 const ActivityFeed = () => {
     return (
         <Card>
@@ -9,6 +10,15 @@ const ActivityFeed = () => {
                     Recent Activity
                 </CardTitle>
             </CardHeader>
+            <CardContent className='space-y-4'>
+                <div className='space-y-4'>
+                    <Avatar className="h-8 w-8 flex-shrink-0">
+                        <AvatarImage src="zarnika/avatar/profile3.png" />
+                        <AvatarFallback>ZN</AvatarFallback>
+                    </Avatar>
+                    
+                </div>
+            </CardContent>
         </Card>
     );
 };
