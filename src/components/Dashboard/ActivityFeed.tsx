3720a288a5147f1f0f849dbe01b2887454644e1e
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-14 23:54:50 +0330

    add icons lucide in file activity feed

--- src/components/Dashboard/ActivityFeed.tsx
+++ src/components/Dashboard/ActivityFeed.tsx
@@ -1,8 +1,16 @@
+import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card';
+import { AlarmClock } from 'lucide-react';
 const ActivityFeed = () => {
     return (
-        <div>Activity</div>
-    )
-}
-
+        <Card>
+            <CardHeader>
+                <CardTitle className="flex items-center gap-2">
+                    <AlarmClock className="h-7 w-7" />
+                    Recent Activity
+                </CardTitle>
+            </CardHeader>
+        </Card>
+    );
+};
 
 export default ActivityFeed;
