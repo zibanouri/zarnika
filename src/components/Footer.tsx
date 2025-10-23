66ae0c3d9939cb7a8be6887272b3e47cf971f53d
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-23 11:33:05 +0330

    add border button color for dashboard header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,24 +8,25 @@ const Footer = ({ sidebarCollapsed }: FooterProps) => {
     return (
         <footer
             className={cn(
-                'bg-background border-t px-6 py-4 w-full transition-all duration-300',
-                sidebarCollapsed ? 'md:pl-16' : 'md:pl-64'
+                'bg-background border-t py-4 transition-all duration-300',
+                sidebarCollapsed ? 'md:pl-16' : 'md:pl-64',
+                'pl-4' 
             )}
         >
-            <div className="text-center text-sm">
-                Zarnika© 2025 Ziba Nouri. All Rights Reserved. Made with love
-                by ✨
-                <a
-                    className="pl-1 text-amber-950 hover:text-amber-700 duration-300"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/zibanouri"
-                >
-                    Ziba
-                </a>
+            <div className="max-w-7xl mx-auto px-2 md:px-4">
+                <div className="text-center text-sm">
+                    Zarnika© 2025 Ziba Nouri. All Rights Reserved. Made with love ✨
+                    <a
+                        className="pl-1 text-amber-950 hover:text-amber-700 duration-300"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/zibanouri"
+                    >
+                       by Ziba
+                    </a>
+                </div>
             </div>
         </footer>
     );
 };
-
 export default Footer;
