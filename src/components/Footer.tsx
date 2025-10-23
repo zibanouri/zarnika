abef4a097a33c36a7fe7f16b90c766db4e11fae5
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-23 14:24:38 +0330

    Animating the footer in the dashboard

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,23 +8,19 @@ const Footer = ({ sidebarCollapsed }: FooterProps) => {
     return (
         <footer
             className={cn(
-                'bg-background border-t py-4 transition-all duration-300',
-                sidebarCollapsed ? 'md:pl-16' : 'md:pl-64',
-                'pl-4' 
+                'fixed bottom-0 right-0 left-0 bg-background border-t border-border duration-300 px-6 py-4',
+                sidebarCollapsed ? 'md:ml-16' : 'md:ml-64'
             )}
         >
-            <div className="max-w-7xl mx-auto px-2 md:px-4">
-                <div className="text-center text-sm">
-                    Zarnika© 2025 Ziba Nouri. All Rights Reserved. Made with love ✨
-                    <a
-                        className="pl-1 text-amber-950 hover:text-amber-700 duration-300"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://github.com/zibanouri"
-                    >
-                       by Ziba
-                    </a>
-                </div>
+            <div className="text-center text-sm">
+                zarnika | Admin Panel © 2025 All rights reserved. Made with ❤️ by
+                <a
+                    className="pl-1.5 text-primary hover:text-primary/70 duration-300"
+                    href="https://github.com/farzadasgari/"
+                    target="_blank"
+                >
+                    Ziba
+                </a>
             </div>
         </footer>
     );
