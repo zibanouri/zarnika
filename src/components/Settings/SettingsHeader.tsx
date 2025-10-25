70aba2257125f377685a28ff47c500589196d637
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-25 23:26:58 +0330

    Begin styling the Settings header

--- src/components/Settings/SettingsHeader.tsx
+++ src/components/Settings/SettingsHeader.tsx
@@ -1,9 +1,14 @@
 const SettingsHeader = () => {
-    return(
-        <div>
-
+  return (
+    <div className="space-y-4">
+      <div className="flex flex-col gap-1">
+        <h1 className="text-2xl font-bold">Settings</h1>
+        <div className="text-sm text-muted-foreground">
+          <span>Last Modified: 2 hours ago by John Doe</span>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default SettingsHeader;
