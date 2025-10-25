6c3be437508cf3baa57081205d7d034960b3b0eb
Ziba Nouri <zibanouri13621115@gmail.com>
2025-10-25 22:26:36 +0330

    initializing settings header components

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,6 +1,10 @@
+import SettingsHeader from "@/components/Settings/settingsHeader";
 const Settings = () => {
-    <div>
-        say hello
+    return(
+    <div className="space-y-6 p-4 md:p-6 overflow-hidden max-w-full">
+       <SettingsHeader />
     </div>
-}
+    );
+};
+
 export default Settings;
---
+++ src/components/Settings/SettingsHeader.tsx
@@ -0,0 +1,9 @@
+const SettingsHeader = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default SettingsHeader;
