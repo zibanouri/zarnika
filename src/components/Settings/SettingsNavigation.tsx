f4cd5f1f8bae484a7dbf19f7ab1e6b4540f7f73f
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-02 22:00:07 +0330

    add setting navigation sample

--- src/components/Settings/SettingsNavigation.tsx
+++ src/components/Settings/SettingsNavigation.tsx
@@ -1,9 +1,25 @@
 import { cn } from '@/lib/utils';
+
+
+const settingsNavigation = [
+    {id:"general",Label:"Grneral"},
+    {id:"user-permition",Label:"User & Permition"},
+    {id:"integrations",Label:"Integrations"},
+    {id:"notifications",Label:"Notifications"},
+    {id:"security",Label:"Security"},
+    {id:"appearance",Label:"Appearance"},
+    {id:"billings",Label:"Billings"},
+    {id:"performance",Label:"Performance"},
+    {id:"advanced",Label:"Advanced"},
+    {id:"audit-log",Label:"Audit Log"},
+]
+
+
 const SettingsNavigation = () => {
     return(
-<div>
-    SettingsNavigation
-</div>
+<nav className='space-y-1'>
+
+</nav>
     )
 }
  export default SettingsNavigation;
