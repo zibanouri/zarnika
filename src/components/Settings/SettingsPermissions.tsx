5f6fe7f6af0961b2fe7a47dfff3c3c05a088ef3f
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-04 21:58:46 +0330

    reformat codes

--- src/components/Settings/SettingsPermissions.tsx
+++ src/components/Settings/SettingsPermissions.tsx
@@ -1,11 +1,15 @@
 const SettingsPermissions = () => {
-    return <div className="space-y-6">
-        <div>
-            <h2 className="text-lg font-semibold mb-2">User & Permissions</h2>
-            <p className="text-muted-foreground">
-                Configure basic organization settings and preferences.
-            </p>
+    return (
+        <div className="space-y-6">
+            <div>
+                <h2 className="text-lg font-semibold mb-2">
+                    User & Permissions
+                </h2>
+                <p className="text-muted-foreground">
+                    Configure basic organization settings and preferences.
+                </p>
+            </div>
         </div>
-    </div>
+    );
 };
 export default SettingsPermissions;
