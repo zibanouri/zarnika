a74bfc2aef66256620591f439c919136c1ab06a6
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-09 20:58:41 +0330

    add circle fading arrow up from lucide react

--- src/components/Settings/SettingsBilling.tsx
+++ src/components/Settings/SettingsBilling.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button';
 import { CircleFadingArrowUp } from 'lucide-react';
 
 
+
 const SettingsBilling = () => {
     return <div>SettingsBilling page</div>;
 };
