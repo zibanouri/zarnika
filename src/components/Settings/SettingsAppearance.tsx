bb1376bb630ae700e586e187184cdc8b5383ffc7
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-09 20:29:43 +0330

    add card , lable , input and switch ui components in settings appearace section

--- src/components/Settings/SettingsAppearance.tsx
+++ src/components/Settings/SettingsAppearance.tsx
@@ -1,3 +1,8 @@
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Switch } from '@/components/ui/switch';
+
 const SettingsAppearance = () => {
     return <div>SettingsAppearance</div>;
 };
