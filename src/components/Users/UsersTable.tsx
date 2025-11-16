a58461ca411a440adfe3e69cde553bb8a74c7c20
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-16 14:12:57 +0330

    add table header to users page

--- src/components/Users/UsersTable.tsx
+++ src/components/Users/UsersTable.tsx
@@ -1,12 +1,12 @@
 import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
 } from "@/components/ui/table"
 
 
@@ -16,7 +16,20 @@ const UsersTable = () => {
         <div className="space-y-4 w-full">userstable
             <div className="border rounded-lg">
                 <div className="overflow-x-auto">
+                    <Table>
+                      <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-12">ID</TableHead>
+                            <TableHead>Role</TableHead>
+                            <TableHead>Department</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Last Login</TableHead>
+                            <TableHead>Actions</TableHead>
+                              <TableHead className="w-32">User</TableHead>
 
+                        </TableRow>
+                      </TableHeader>
+                    </Table>
                 </div>
             </div>
         </div>
