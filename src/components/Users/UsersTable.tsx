ca24a1d81e1f28c901155b8e29e5d25391c1dcdc
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-16 13:56:59 +0330

    add table ui in code

---
+++ src/components/Users/UsersTable.tsx
@@ -0,0 +1,26 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+
+
+const UsersTable = () => {
+    return (
+        <div className="space-y-4 w-full">userstable
+            <div className="border rounded-lg">
+                <div className="overflow-x-auto">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UsersTable;
