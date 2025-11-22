0039cb1b17ed796a1f17f79bc51cd8738b64126d
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-22 16:16:57 +0330

    add avatar ui in code

--- src/components/Users/AddEditUserModal.tsx
+++ src/components/Users/AddEditUserModal.tsx
@@ -1,4 +1,6 @@
-import { Dialog, DialogContent, DialogHeader,DialogTitle } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
+
 interface AddEditUserModalProps {
     isOpen: boolean;
     onClose: () => void;
@@ -8,9 +10,24 @@ const AddEditUserModal = ({ isOpen, onClose }: AddEditUserModalProps) => {
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
             <DialogContent className="max-w-md max-h-[90vh] overflow-y-hidden">
-                <DialogHeader><DialogTitle>
+                <DialogHeader>
+                    <DialogTitle>
+                        Add new user
                     </DialogTitle>
-                    </DialogHeader>
+                </DialogHeader>
+
+
+                <form action="javascript:void(0)" className='space-y-4'>
+                    <div className='flex flex-col items-center gap-3'>
+                        <Avatar className='w-20 h-20'>
+                            <AvatarImage>
+                                <AvatarFallback>
+                                    Ar
+                                </AvatarFallback>
+                            </AvatarImage>
+                        </Avatar>
+                    </div>
+                </form>
             </DialogContent>
         </Dialog>
     );
