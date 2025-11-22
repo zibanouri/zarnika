e44e1cfcf3e7b8b8a781ee86887bdc85190a3d11
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-22 17:15:17 +0330

    add select ui in code

--- src/components/Users/AddEditUserModal.tsx
+++ src/components/Users/AddEditUserModal.tsx
@@ -1,7 +1,10 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
-import {Button} from '@/components/ui/button';
-import {Upload} from 'lucide-react'
+import { Button } from '@/components/ui/button';
+import { Upload } from 'lucide-react'
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input'
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select'
 
 interface AddEditUserModalProps {
     isOpen: boolean;
@@ -19,20 +22,47 @@ const AddEditUserModal = ({ isOpen, onClose }: AddEditUserModalProps) => {
                 </DialogHeader>
 
 
-                <form action="javascript:void(0)" className='space-y-4'>
-                    <div className='flex flex-col items-center gap-3'>
-                        <Avatar className='w-20 h-20'>
-                            <AvatarImage src='zarnika/avatar/image1.webp'/>
-                                <AvatarFallback className='text-lg'>
-                                    Ar
-                                </AvatarFallback>
-                          
+                <form action="javascript:void(0)" className="space-y-4">
+                    <div className="flex flex-col items-center gap-3">
+                        <Avatar className="w-20 h-20">
+                            <AvatarImage src="zarnika/avatar/image1.webp" />
+                            <AvatarFallback className="text-lg">Ar</AvatarFallback>
                         </Avatar>
-                        <Button type='button' variant="outline" size="sm" className='cursor-pointer'>
-                            <Upload className='h-4 w-4 mr-2'/>Upload photo
+                        <Button type="button" variant="outline" size="sm" className="cursor-pointer">
+                            <Upload className="h-4 w-4 mr-2" />
+                            Upload photo
                         </Button>
                     </div>
+
+                    <div className="space-y-2">
+                        <Label htmlFor="name">Full Name</Label>
+                        <Input id="name" type="text" name='name' placeholder='Inter full name' />
+                    </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" type="text" name='email' placeholder='Inter email address' />
+                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className='space-y-4'>
+                            <Label htmlFor="">Role</Label>
+                            <Select>
+                                <SelectTrigger className='w-100'>
+                                    <SelectValue placeholder="Select role"></SelectValue>
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="Admin">َAdmin</SelectItem>
+                                    <SelectItem value="Editor">َEditor</SelectItem>
+                                    <SelectItem value="Viewer">َViewer</SelectItem>
+
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <SelectItem value="option1">Option 1</SelectItem>
+                        <SelectItem value="option2">Option 2</SelectItem>
+                        <SelectItem value="option3">Option 3</SelectItem>
+                    </div>
                 </form>
+
             </DialogContent>
         </Dialog>
     );
