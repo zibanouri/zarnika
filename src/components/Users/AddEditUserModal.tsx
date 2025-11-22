faf1b0ff2e0b1d15246f9421eddd0f4ae249b1f6
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-22 16:32:24 +0330

    add upload button to add new user modal

--- src/components/Users/AddEditUserModal.tsx
+++ src/components/Users/AddEditUserModal.tsx
@@ -1,5 +1,7 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
+import {Button} from '@/components/ui/button';
+import {Upload} from 'lucide-react'
 
 interface AddEditUserModalProps {
     isOpen: boolean;
@@ -20,12 +22,15 @@ const AddEditUserModal = ({ isOpen, onClose }: AddEditUserModalProps) => {
                 <form action="javascript:void(0)" className='space-y-4'>
                     <div className='flex flex-col items-center gap-3'>
                         <Avatar className='w-20 h-20'>
-                            <AvatarImage>
-                                <AvatarFallback>
+                            <AvatarImage src='zarnika/avatar/image1.webp'/>
+                                <AvatarFallback className='text-lg'>
                                     Ar
                                 </AvatarFallback>
-                            </AvatarImage>
+                          
                         </Avatar>
+                        <Button type='button' variant="outline" size="sm" className='cursor-pointer'>
+                            <Upload className='h-4 w-4 mr-2'/>Upload photo
+                        </Button>
                     </div>
                 </form>
             </DialogContent>
